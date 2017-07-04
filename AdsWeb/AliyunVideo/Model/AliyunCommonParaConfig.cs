7646f908e11102f76f194e445187d3bc8c1eb73e
jorzen <jorzen2010@163.com>
2017-07-04 16:31:29 +0800

    aliyun

---
+++ AdsWeb/AliyunVideo/Model/AliyunCommonParaConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdsWeb.AliyunVideo
+{
+    public class AliyunCommonParaConfig
+    {
+
+        public const string Format = "JSON";
+        public const string Version = "2017-03-21";
+        public const string AccessKeyId = "LTAIytsYvH3VQbXY";
+        public const string AccessKeySecret = "ArizByHVXy0rMTwREIVhnzvpZiwSGg";
+
+
+
+        public const string SignatureVersion = "1.0";
+
+
+        public const string SignatureMethod = "HMAC-SHA1";
+
+
+    }
+}
+
+
---
+++ AdsWeb/AliyunVideo/business/AliyunVideoServices.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+
+namespace AdsWeb.AliyunVideo
+
+{
+    public class AliyunVideoServices
+    {
+        public static string GetVideoPlayAuth()
+        {
+            string PlayAuth = "";
+            return PlayAuth;
+        }
+
+      
+        public static string GetSign()
+        {
+            string Sign = "";
+            return Sign;
+        
+        }
+
+
+        public static string GetStringToSign()
+        {
+            //字典排序，就是需要将那个值进行分割，分割后再重新组合，就行了。
+            Dictionary<string, string> dics = new Dictionary<string, string>();
+            dics.Add("Format", AliyunCommonParaConfig.Format);
+            dics.Add("Version", AliyunCommonParaConfig.Version);
+            dics.Add("AccessKeyId", AliyunCommonParaConfig.AccessKeyId);
+            dics.Add("SignatureVersion", AliyunCommonParaConfig.SignatureVersion);
+            dics.Add("SignatureMethod", AliyunCommonParaConfig.SignatureMethod);
+
+           
+            return  HttpUtility.UrlEncode(getParamSrc(dics));
+        }
+
+        public static String getParamSrc(Dictionary<string, string> paramsMap)
+        {
+            var vDic = (from objDic in paramsMap orderby objDic.Key ascending select objDic);
+            StringBuilder str = new StringBuilder();
+            foreach (KeyValuePair<string, string> kv in vDic)
+            {
+                string pkey = kv.Key;
+                string pvalue = kv.Value;
+                str.Append(pkey + "=" + pvalue + "&");
+            }
+
+            String result = str.ToString().Substring(0, str.ToString().Length - 1);
+            return result;
+        }
+
+    }
+}
