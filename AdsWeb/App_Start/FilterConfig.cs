1b6678c213ed6602b5e00c432ba9299d5b3f8417
jorzen <jorzen2010@163.com>
2017-07-21 16:39:19 +0800

    quxnain

--- AdsWeb/App_Start/FilterConfig.cs
+++ AdsWeb/App_Start/FilterConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using AdsWeb.Attributes;
 
 namespace AdsWeb
 {
@@ -8,6 +9,8 @@ namespace AdsWeb
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new CustomAuthorizeAttribute());
         }
+        
     }
 }
---
+++ AdsWeb/Attributes/CustomAuthorizeAttribute.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Collections;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using AdsEntity;
+using AdsDal;
+
+namespace AdsWeb.Attributes
+{
+    public class CustomAuthorizeAttribute : AuthorizeAttribute
+    {
+        private SkyWebContext db = new SkyWebContext();
+        // 只需重载此方法，模拟自定义的角色授权机制  
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            string controllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
+            string actionName = filterContext.ActionDescriptor.ActionName;
+       
+            base.OnAuthorization(filterContext);
+        }
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+
+            if (httpContext == null)
+            {
+                throw new ArgumentException("HttpContext");
+            }
+
+           
+
+            if (!httpContext.User.Identity.IsAuthenticated)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            
+            }
+        }
+
+    }
+}
